2f8f673a0d11c1e8ca807a69d4f6956640d10527
shyu <shyu6370@gmail.com>
2024-04-16 21:12:57 +0900

    맛집에 등록된 리뷰 가져오기 API 추가

--- src/main/java/com/jyujyu/review/api/ReviewApi.java
+++ src/main/java/com/jyujyu/review/api/ReviewApi.java
@@ -1,8 +1,11 @@
 package com.jyujyu.review.api;
 
 import com.jyujyu.review.api.request.CreateReviewRequest;
+import com.jyujyu.review.model.dto.ReviewDto;
 import com.jyujyu.review.service.ReviewService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -19,5 +22,13 @@ public class ReviewApi {
     @DeleteMapping("/review/{reviewId}")
     public void deleteReview(@PathVariable("reviewId") Long reviewId) {
         reviewService.deleteReview(reviewId);
+    }
+
+    @GetMapping("/restaurant/{restaurantId}/reviews")
+    public ReviewDto getRestaurantReviews(@PathVariable("restaurantId") Long restaurantId,
+                                          @RequestParam("offset") Integer offset,
+                                          @RequestParam("limit") Integer limit
+    ) {
+        return reviewService.getRestaurantReview(restaurantId, PageRequest.of(offset / limit, limit));
     }
 }
